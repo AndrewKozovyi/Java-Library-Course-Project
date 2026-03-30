b7af63c048f0c3048c1998aff5320322a99a0fc5
Andrew <andriy.kozovyi@gmail.com>
2026-03-30 21:05:50 +0300

    changed link to backend

--- Library FE/src/services/book.service.ts
+++ Library FE/src/services/book.service.ts
@@ -7,7 +7,7 @@ import { Book } from '../models/book.model';
   providedIn: 'root'
 })
 export class BookService {
-  private apiUrl = 'http://localhost:8080/api/books';
+  private apiUrl = 'https://java-library-course-project.onrender.com/api/books';
 
   constructor(private http: HttpClient) {}
 
